0dcfd5ccffc1db3e9e2a679a98f677a203a60fe2
Tatyana Kozlova <tatyana.y.kozlova@raiffeisen.ru>
2021-04-03 19:09:46 +0300

    create field model and test

---
+++ test/io/hexlet/xo/model/FieldTest.java
@@ -0,0 +1,32 @@
+package io.hexlet.xo.model;
+
+import org.junit.jupiter.api.Test;
+
+import java.awt.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FieldTest {
+
+    @Test
+    void getSize() {
+        final Field field = new Field();
+
+        assertEquals(3, field.getSize());
+    }
+
+
+    @Test
+    void setFigure() {
+        final Field field = new Field();
+        final Point point = new Point(0, 0);
+        final Figure figure = Figure.O;
+
+        field.setFigure(point, figure);
+        final Figure actualFigure = field.getFigure(point);
+        final Figure expectedFigure = Figure.O;
+
+        assertEquals(expectedFigure, actualFigure);
+
+    }
+}
