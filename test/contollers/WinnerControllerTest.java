4cca064269b3240ab53cac94cc5991dac7d2d2da
Tatyana Kozlova <tatyana.y.kozlova@raiffeisen.ru>
2021-04-07 21:14:10 +0300

    tests for winner controller

--- test/contollers/WinnerControllerTest.java
+++ test/contollers/WinnerControllerTest.java
@@ -7,8 +7,7 @@ import org.junit.jupiter.api.Test;
 
 import java.awt.*;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 class WinnerControllerTest {
 
@@ -160,7 +159,7 @@ class WinnerControllerTest {
             for (int j = 0; j < fieldSize; j++) {
                 field.setFigure(new Point(j, row), figure);
             }
-            field.setFigure(new Point( fieldSize - 2, row), Figure.X);
+            field.setFigure(new Point(fieldSize - 2, row), Figure.X);
         } catch (InvalidPointException e) {
             e.printStackTrace();
         }
@@ -191,4 +190,162 @@ class WinnerControllerTest {
         assertNull(winner);
     }
 
+    @Test
+    void getWinner_WhenWinner_InDiagonal1() {
+        WinnerController wc = new WinnerController();
+        Figure figure = Figure.O;
+        Field field = new Field(fieldSize);
+
+        int end = field.getSize() - 1;
+
+        for (int start = 0; start < field.getSize(); start++) {
+            final Point p = new Point();
+            p.x = start;
+            p.y = end;
+
+            try {
+                field.setFigure(p, figure);
+            } catch (InvalidPointException e) {
+                e.printStackTrace();
+                fail();
+            }
+            end--;
+        }
+
+        Figure winner = wc.getWinner(field);
+        System.out.println(field.toString());
+        assertEquals(figure, winner);
+    }
+
+    @Test
+    void getWinner_WhenOtherFigure_InDiagonal1() {
+        WinnerController wc = new WinnerController();
+        Figure figure = Figure.O;
+        Field field = new Field(fieldSize);
+
+        int end = field.getSize() - 1;
+
+        for (int start = 0; start < field.getSize(); start++) {
+            final Point p = new Point();
+            p.x = start;
+            p.y = end;
+
+            try {
+                field.setFigure(p, figure);
+            } catch (InvalidPointException e) {
+                e.printStackTrace();
+                fail();
+            }
+            end--;
+        }
+        try {
+            field.setFigure(new Point(0, fieldSize - 1), Figure.X);
+        } catch (InvalidPointException e) {
+            e.printStackTrace();
+            fail();
+        }
+
+        Figure winner = wc.getWinner(field);
+        System.out.println(field.toString());
+        assertNull(winner);
+    }
+
+    @Test
+    void getWinner_WhenWinner_InDiagonal2() {
+        WinnerController wc = new WinnerController();
+        Figure figure = Figure.O;
+        Field field = new Field(fieldSize);
+
+        for (int i = 0; i < field.getSize(); i++) {
+            final Point p = new Point();
+            p.x = i;
+            p.y = i;
+
+            try {
+                field.setFigure(p, figure);
+            } catch (InvalidPointException e) {
+                e.printStackTrace();
+                fail();
+            }
+        }
+
+        Figure winner = wc.getWinner(field);
+        System.out.println(field.toString());
+        assertEquals(figure, winner);
+    }
+
+
+    @Test
+    void getWinner_WhenOtherFigure_InDiagonal2() {
+        WinnerController wc = new WinnerController();
+        Figure figure = Figure.O;
+        Field field = new Field(fieldSize);
+
+        for (int i = 0; i < field.getSize(); i++) {
+            final Point p = new Point();
+            p.x = i;
+            p.y = i;
+
+            try {
+                field.setFigure(p, figure);
+            } catch (InvalidPointException e) {
+                e.printStackTrace();
+                fail();
+            }
+        }
+        try {
+            field.setFigure(new Point(1, 1), Figure.X);
+        } catch (InvalidPointException e) {
+            e.printStackTrace();
+            fail();
+        }
+
+        Figure winner = wc.getWinner(field);
+        System.out.println(field.toString());
+        assertNull(winner);
+    }
+
+    @Test
+    void check_Point_LessThanNull() {
+        WinnerController winnerController = new WinnerController();
+        boolean result = winnerController.check(
+                new Field(fieldSize),
+                new Point(-1, 0),
+                p -> new Point(p.x + 1, p.y + 1)
+        );
+        assertFalse(result);
+    }
+
+    @Test
+    void check_Point_MoreThanFieldSize() {
+        WinnerController winnerController = new WinnerController();
+        boolean result = winnerController.check(
+                new Field(fieldSize),
+                new Point(1, fieldSize + 1),
+                p -> new Point(p.x + 1, p.y)
+        );
+        assertFalse(result);
+    }
+
+    @Test
+    void check_Winner() {
+        WinnerController winnerController = new WinnerController();
+        Field field = new Field(fieldSize);
+
+        for (int j = 0; j < fieldSize; j++) {
+            try {
+                field.setFigure(new Point(j, 0), Figure.X);
+            } catch (InvalidPointException e) {
+                e.printStackTrace();
+                fail();
+            }
+        }
+
+        boolean result = winnerController.check(
+                field,
+                new Point(0, 0),
+                p -> new Point(p.x + 1, p.y)
+        );
+        assertTrue(result);
+    }
 }
